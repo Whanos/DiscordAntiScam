63ff679617aa0cf8830c2238f757ea6a618beb79
Whanos <github@sergal.fun>
2021-12-07 13:40:09 +0000

    quite the change

---
+++ FsAntiScamFilter/FsAntiScamFilter/FsAntiScamFilter.c
@@ -0,0 +1,164 @@
+/*++
+
+Module Name:
+
+    FsAntiScamFilter.c
+
+Abstract:
+
+    This is the main module of the FsAntiScamFilter miniFilter driver.
+
+Environment:
+
+    Kernel mode
+
+--*/
+
+#include <fltKernel.h>
+#include <dontuse.h>
+#include <suppress.h>
+
+PFLT_FILTER FilterHandle = NULL;
+
+NTSTATUS FsFilterUnload(FLT_FILTER_UNLOAD_FLAGS Flags);
+// IRP_MJ_CREATE
+FLT_PREOP_CALLBACK_STATUS FsFilterPreCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext);
+FLT_POSTOP_CALLBACK_STATUS FsFilterPostCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext, FLT_POST_OPERATION_FLAGS Flags);
+// IRP_MJ_READ
+FLT_PREOP_CALLBACK_STATUS FsFilterPreRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext);
+FLT_POSTOP_CALLBACK_STATUS FsFilterPostRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext, FLT_POST_OPERATION_FLAGS Flags);
+// The operations we want to filter
+const FLT_OPERATION_REGISTRATION FilterCallbacks[] = {
+    {IRP_MJ_CREATE, 0, FsFilterPreCreate, FsFilterPostCreate},
+    {IRP_MJ_READ, 0, FsFilterPreRead, FsFilterPostRead},
+    {IRP_MJ_OPERATION_END}
+};
+
+// Filter details to register with
+const FLT_REGISTRATION FilterRegistration = {
+    sizeof(FLT_REGISTRATION),   // Size
+    FLT_REGISTRATION_VERSION,   // Version
+    0,                          // Flags
+    NULL,                       // *ContextRegistration
+    FilterCallbacks,            // *OperationRegistration
+    FsFilterUnload,             // FilterUnloadCallback (Unload Function)
+    NULL,                       // InstanceSetupCallback
+    NULL,                       // InstanceQueryTeardownCallback
+    NULL,                       // InstanceTeardownStartCallback
+    NULL,                       // InstanceTeardownCompleteCallback
+    NULL,                       // GenerateFileNameCallback
+    NULL,                       // NormalizeNameComponentCallback
+    NULL,                       // NormalizeContextCleanupCallback
+    NULL,                       // TransactionNotificationCallback
+};              
+
+FLT_PREOP_CALLBACK_STATUS FsFilterPreRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext) {
+
+    UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(FltObjects);
+    
+    PFLT_FILE_NAME_INFORMATION FileNameInfo;
+    NTSTATUS status;
+    WCHAR FileName[400] = { 0 };
+
+    status = FltGetFileNameInformation(Data, FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT, &FileNameInfo);
+
+    if (NT_SUCCESS(status)) {
+        status = FltParseFileNameInformation(FileNameInfo);
+        if (NT_SUCCESS(status)) {
+            if (FileNameInfo->Name.MaximumLength < 400) {
+                RtlCopyMemory(FileName, FileNameInfo->Name.Buffer, FileNameInfo->Name.MaximumLength);
+                _wcsupr(FileName);
+                KdPrint(("File PreRead: %ws \r\n", FileName)); // Spammy af line
+                if (wcsstr(FileName, L"CANTREADME.TXT") != NULL) {
+                    KdPrint(("Can't read that! (denied) \r\n"));
+                    Data->IoStatus.Status = STATUS_ACCESS_DENIED;
+                    Data->IoStatus.Information = 0;
+                    FltReleaseFileNameInformation(FileNameInfo);
+                    return FLT_PREOP_COMPLETE;
+                }
+            }
+        }
+    }
+
+    return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+}
+
+FLT_POSTOP_CALLBACK_STATUS FsFilterPostRead(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext, FLT_POST_OPERATION_FLAGS Flags) {
+   
+    UNREFERENCED_PARAMETER(Flags);
+    UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(Data);
+
+    return FLT_POSTOP_FINISHED_PROCESSING;
+}
+
+FLT_POSTOP_CALLBACK_STATUS FsFilterPostCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext, FLT_POST_OPERATION_FLAGS Flags) {
+    
+    UNREFERENCED_PARAMETER(Flags);
+    UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(FltObjects);
+    UNREFERENCED_PARAMETER(Data);
+    
+    return FLT_POSTOP_FINISHED_PROCESSING;
+}
+
+
+// PreCreate routine handler
+FLT_PREOP_CALLBACK_STATUS FsFilterPreCreate(PFLT_CALLBACK_DATA Data, PCFLT_RELATED_OBJECTS FltObjects, PVOID* CompletionContext) {
+
+    UNREFERENCED_PARAMETER(CompletionContext);
+    UNREFERENCED_PARAMETER(FltObjects);
+
+    PFLT_FILE_NAME_INFORMATION FileNameInfo;
+    NTSTATUS status;
+    WCHAR FileName[400] = { 0 };
+    
+    status = FltGetFileNameInformation(Data, FLT_FILE_NAME_NORMALIZED | FLT_FILE_NAME_QUERY_DEFAULT, &FileNameInfo);
+
+    if (NT_SUCCESS(status)) {
+        status = FltParseFileNameInformation(FileNameInfo);
+
+        if (NT_SUCCESS(status)) {
+            if (FileNameInfo->Name.MaximumLength < 400) {
+                RtlCopyMemory(FileName, FileNameInfo->Name.Buffer, FileNameInfo->Name.MaximumLength);
+                //KdPrint(("File PreCreate: %ws \r\n", FileName));
+            }
+        }
+        FltReleaseFileNameInformation(FileNameInfo);
+    }
+
+    return FLT_PREOP_SUCCESS_WITH_CALLBACK;
+}
+
+// Unload routine for the minifilter.
+NTSTATUS FsFilterUnload(FLT_FILTER_UNLOAD_FLAGS Flags) {
+
+    UNREFERENCED_PARAMETER(Flags);
+
+    KdPrint(("Unloading FsAntiScam filter driver! \r\n"));
+    FltUnregisterFilter(FilterHandle);
+
+    return STATUS_SUCCESS;
+}
+
+
+// Entry point for the minifilter.
+NTSTATUS DriverEntry(PDRIVER_OBJECT DriverObject, PUNICODE_STRING RegistryPath) {
+    NTSTATUS status;
+
+    UNREFERENCED_PARAMETER(RegistryPath);
+
+    status = FltRegisterFilter(DriverObject, &FilterRegistration, &FilterHandle);
+
+    if (NT_SUCCESS(status)) {
+        status = FltStartFiltering(FilterHandle);
+
+        if (!NT_SUCCESS(status)) {
+            FltUnregisterFilter(FilterHandle);
+        }
+    }
+
+    return status;
+}
