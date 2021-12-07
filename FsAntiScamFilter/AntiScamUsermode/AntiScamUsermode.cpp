caf40aacaf53a8c40f1025449ff86443d82115ed
Whanos <github@sergal.fun>
2021-12-07 21:17:44 +0000

    fuck i hate programming - whanos

--- FsAntiScamFilter/AntiScamUsermode/AntiScamUsermode.cpp
+++ FsAntiScamFilter/AntiScamUsermode/AntiScamUsermode.cpp
@@ -7,24 +7,24 @@
 #pragma comment(lib, "kernel32.lib")
 #pragma comment(lib, "fltlib.lib")
 
-HANDLE Port = NULL;
-
 int main()
 {
+    HANDLE Port = INVALID_HANDLE_VALUE;
+    HRESULT result = S_OK;
+
     std::cout << "AntiScam usermode client\n";
-    std::cout << "Attempting to establish connection with filter port "<< PortName << "\n";
+    std::cout << "Attempting to establish connection with filter port...\n";
 
     DWORD BytesReceived = 0;
     PCHAR MessageBuffer = "Message";
     char ReceiveBuffer[500] = { 0 };
 
-    if (Port == NULL) {
-        HRESULT result = FilterConnectCommunicationPort(L"\\FsAntiScamPort", 0, NULL, 0, NULL, &Port);
-        if (result) {
-            std::cout << "Couldn't connect to filter!";
-            return 0;
-         }
+    result = FilterConnectCommunicationPort(L"\\FsAntiScamPort", 0, NULL, 0, NULL, &Port);
+    if (IS_ERROR(result)) {
+        printf("Could not connect to filter! :( - 0x%08x\n", result);
+        return 0;
     }
+    printf("Connected to filter!\n");
     if (FilterSendMessage(Port, MessageBuffer, strlen(MessageBuffer), ReceiveBuffer, 500, &BytesReceived)) {
         std::cout << ReceiveBuffer << "\n";
     }
